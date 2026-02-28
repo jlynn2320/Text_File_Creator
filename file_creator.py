3a93c35b47f0679bf7576330b13679b43d0b32b3
jlynn2320 <jlynn2320@gmail.com>
2026-02-28 01:14:10 -0500

    Text_File_Creator

---
+++ file_creator.py
@@ -0,0 +1,7 @@
+name = input("what's your name? \n")
+color = input("what's your favorite color? \n")
+with open('example.txt', 'w') as file:
+        file.write(f"{Jackie} created this file")
+        file.write(f"\n Favorite Color:{purple}")
+          
+    
