13f9144e435e39c28c456639f30b8328f58fe2f1
jlynn2320 <jlynn2320@gmail.com>
2026-02-28 17:39:44 -0500

    updating code for Text File Creator

--- file_creator.py
+++ file_creator.py
@@ -1,7 +1,6 @@
-name = input("what's your name? \n")
+name = input("What's your name? \n")
 color = input("what's your favorite color? \n")
 with open('example.txt', 'w') as file:
-        file.write(f"{Jackie} created this file")
-        file.write(f"\n Favorite Color:{purple}")
+    file.write(f"{name} created this file")
+    file.write(f"\n{color} favorite color")
           
-    
